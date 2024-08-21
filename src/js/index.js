b9ae714b892d1b2507c311bb0ff0f638b23e3df6
Cristian Ayala Sánchez <ayalasanchezcristian2000@gmail.com>
2024-08-21 17:39:55 +0000

    proyecto task-list con API finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+<script src="https://kit.fontawesome.com/2a04b1758b.js" crossorigin="anonymous"></script>
+
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -10,4 +14,3 @@ import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
